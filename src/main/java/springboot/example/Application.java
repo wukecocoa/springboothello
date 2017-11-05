06caf9095ea06213c248fd3d74e440f40573cdec
wukecocoa <31623980+wukecocoa@users.noreply.github.com>
2017-11-05 20:51:23 +0800

    Initial commit

---
+++ src/main/java/springboot/example/Application.java
@@ -0,0 +1,24 @@
+package springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application  {
+    @RequestMapping("/")
+    String home(){
+        return  "hello";
+    }
+
+    public static void main(String [] args){
+        SpringApplication.run(Application.class,args);
+    }
+
+//    @Override
+//    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+//        configurableEmbeddedServletContainer.setPort(8888);
+//    }
+}
